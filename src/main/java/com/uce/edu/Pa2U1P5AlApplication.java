ef826cfb5c1928833ce547cd59bd47fa46ef2214
AndresLaraV <aslarav@uce.edu.ec>
2023-11-13 19:40:42 -0500

    Taller 5

--- src/main/java/com/uce/edu/Pa2U1P5AlApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AlApplication.java
@@ -1,11 +1,18 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5AlApplication implements CommandLineRunner{
+	@Autowired	
+	private Estudiante estudiante;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5AlApplication.class, args);
@@ -14,14 +21,13 @@ public class Pa2U1P5AlApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
-		System.out.println("Soy Andres Lara");
+		this.estudiante.setNombre("Andres");
+		this.estudiante.setApellido("Lara");
+		this.estudiante.setCedula("1722121835");
+		this.estudiante.setSalario(new BigDecimal(100));
 		
-		//PRUEBA		
-		System.out.println("mi segundo commit");		
-		System.out.println("cambio en nueva rama taller3");
+		System.out.println(estudiante);
 		
-		System.out.println("taller 4.0.1");
+		
 	}
 }
