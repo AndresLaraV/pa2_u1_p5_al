8748bce225e58d28fdaee0f73a936aa2e02b2bf1
AndresLaraV <aslarav@uce.edu.ec>
2023-11-17 19:51:39 -0500

    taller 7

--- src/main/java/com/uce/edu/Pa2U1P5AlApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AlApplication.java
@@ -1,19 +1,22 @@
 package com.uce.edu;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.ioc.di.Estudiante;
+import com.uce.edu.repository.modelo.Materia;
+import com.uce.edu.service.IMateriaService;
 
 @SpringBootApplication
 public class Pa2U1P5AlApplication implements CommandLineRunner{
-	@Autowired	
-	private Estudiante estudiante;
-
+	@Autowired
+	private IMateriaService iMateriaService;
+	@Autowired
+	private Materia materia;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5AlApplication.class, args);
 		
@@ -21,13 +24,30 @@ public class Pa2U1P5AlApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		this.estudiante.setNombre("Andres");
-		this.estudiante.setApellido("Lara");
-		this.estudiante.setCedula("1722121835");
-		this.estudiante.setSalario(new BigDecimal(100));
 		
-		System.out.println(estudiante);
+		this.materia.setCodigo("M1");
+		this.materia.setNombre("P-Avanzada");
+		this.materia.setNumeroCreditos(10);
+		this.iMateriaService.registrar(materia);
+	    
+		System.out.println("Reporte Inico");
+		List<Materia> reporte = this.iMateriaService.selecionarTodos();
+		for (Materia mate : reporte) {
+			System.out.println(mate);
+			
+		}
 		
+		Materia mate=this.iMateriaService.buscar("M1");
+		System.out.println(mate);
+		
+		mate.setNumeroCreditos(30);
+		this.iMateriaService.actualizar(mate);
+		Materia mate2=this.iMateriaService.buscar("M1");
+		System.out.println(mate2);
+		
+		this.iMateriaService.eliminar("M1");
+		mate2 = this.iMateriaService.buscar("M1");
+		System.out.println(mate2);
 		
 	}
 }
