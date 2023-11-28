41f3a5a8f0fc260a31409fb466460e59188ee083
AndresLaraV <aslarav@uce.edu.ec>
2023-11-27 23:51:56 -0500

    Tarea5

---
+++ pa2_u1_p5_al-taller10/src/main/java/com/uce/edu/Pa2U1P5AlApplication.java
@@ -0,0 +1,63 @@
+package com.uce.edu;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.uce.edu.transferencia.repository.modelo.CuentaBancaria;
+import com.uce.edu.transferencia.repository.modelo.Transferencia;
+import com.uce.edu.transferencia.service.ICuentaBancariaService;
+import com.uce.edu.transferencia.service.ITransferenciaService;
+
+@SpringBootApplication
+public class Pa2U1P5AlApplication implements CommandLineRunner {
+	@Autowired
+	private ITransferenciaService iTransferenciaService;
+	@Autowired
+	private ICuentaBancariaService bancariaService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5AlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//1. Crear las cuentas
+		CuentaBancaria ctaOrigen = new CuentaBancaria();
+		ctaOrigen.setCedulaPropietario("1722121835");
+		ctaOrigen.setNumero("1234");
+		ctaOrigen.setSaldo(new BigDecimal (100));
+		this.bancariaService.guardar(ctaOrigen);
+		
+		CuentaBancaria ctaDestino = new CuentaBancaria();
+		ctaDestino.setCedulaPropietario("1704983491");
+		ctaDestino.setNumero("6789");
+		ctaDestino.setSaldo(new BigDecimal(200));
+		this.bancariaService.guardar(ctaDestino);
+		
+		this.iTransferenciaService.realizar("1234", "6789", new BigDecimal(20));
+		this.iTransferenciaService.realizar("1234", "6789", new BigDecimal(20));
+		this.iTransferenciaService.realizar("1234", "6789", new BigDecimal(20));
+		this.iTransferenciaService.realizar("1234", "6789", new BigDecimal(20));
+		this.iTransferenciaService.realizar("1234", "6789", new BigDecimal(20));
+	
+		CuentaBancaria ctaOrigen1 = this.bancariaService.buscar("1234");
+		System.out.println(ctaOrigen1);
+		CuentaBancaria ctaDestino1 = this.bancariaService.buscar("6789");
+		System.out.println(ctaDestino1);
+		
+		List<Transferencia> lista = this.iTransferenciaService.buscarTodos();
+
+
+		int indice = 0;
+		for (Transferencia trans:lista) {
+			indice++;
+			System.out.println(indice+ ": "+trans);
+		}
+	}
+}
---
+++ pa2_u1_p5_al-taller10/src/main/java/com/uce/edu/transferencia/service/TransferenciaServiceImpl.java
@@ -0,0 +1,86 @@
+package com.uce.edu.transferencia.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uce.edu.transferencia.repository.ICuentaBancariaRepository;
+import com.uce.edu.transferencia.repository.ITransferenciaRepository;
+import com.uce.edu.transferencia.repository.modelo.CuentaBancaria;
+import com.uce.edu.transferencia.repository.modelo.Transferencia;
+
+@Service
+public class TransferenciaServiceImpl implements ITransferenciaService {
+	@Autowired
+	private ICuentaBancariaRepository bancariaRepository;
+	@Autowired
+	private ITransferenciaRepository iTransferenciaRepository;
+	@Override
+	public Transferencia buscar(String numero) {
+		// TODO Auto-generated method stub
+		return this.iTransferenciaRepository.seleccionar(numero);
+	}
+	@Override
+	public void guardar(Transferencia transferencia) {
+		// TODO Auto-generated method stub
+		this.iTransferenciaRepository.insertar(transferencia);
+	}
+	@Override
+	public void actualizar(Transferencia transferencia) {
+		// TODO Auto-generated method stub
+		this.iTransferenciaRepository.actualizar(transferencia);
+	}
+	@Override
+	public void realizar(String numeroOrigen, String numeroDestino, BigDecimal monto) {
+		CuentaBancaria ctaOrigen = this.bancariaRepository.seleccionar(numeroOrigen);
+		BigDecimal saldoOrigen = ctaOrigen.getSaldo();
+		if (saldoOrigen.compareTo(monto) >= 0) {
+			BigDecimal nuevoSaldoOrigen = saldoOrigen.subtract(monto);
+			ctaOrigen.setSaldo(nuevoSaldoOrigen);
+			this.bancariaRepository.actualizar(ctaOrigen);
+			CuentaBancaria ctaDestino = this.bancariaRepository.seleccionar(numeroDestino);
+			BigDecimal saldoDestino = ctaDestino.getSaldo();
+			BigDecimal nuevoSaldoDestino = saldoDestino.add(monto);
+			ctaDestino.setSaldo(nuevoSaldoDestino);
+			this.bancariaRepository.actualizar(ctaDestino);
+			Transferencia transferencia = new Transferencia();
+			transferencia.setCuentaOrigen(ctaOrigen);
+			transferencia.setCuentaDestino(ctaDestino);
+			transferencia.setFecha(LocalDateTime.now());
+			transferencia.setMonto(monto);		
+			transferencia.setMonto(monto);
+
+			List<Transferencia> lista = this.iTransferenciaRepository.seleccionarTodos();
+
+
+			int indice = 1;
+			for (Transferencia trans:lista) {
+				indice++;
+				String numString = String.valueOf(indice);
+				transferencia.setNumero(numString);			
+
+			}
+
+
+			this.iTransferenciaRepository.insertar(transferencia);
+			System.out.println("Transferenia realizada con Exito!");
+		} else {
+			System.out.println("Saldo no disponible");
+		}
+	}
+
+
+	@Override
+	public List<Transferencia> buscarTodos() {
+		// TODO Auto-generated method stub
+		return this.iTransferenciaRepository.seleccionarTodos();
+	}
+	@Override
+	public void eliminar(String numero) {
+		// TODO Auto-generated method stub
+		this.iTransferenciaRepository.eliminar(numero);
+	}
+}
